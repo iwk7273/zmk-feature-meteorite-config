06c4cb629a07a77efd13df236ea208e509402298
iwk7273 <iwk.nov16@gmail.com>
2026-02-11 15:09:02 +0900

    Add meteorite custom key behavior

---
+++ include/dt-bindings/zmk/meteorite_custom_keys.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#pragma once
+
+#define M_OS_CTRL_CMD 1
+#define M_OS_ALT_OPT 2
+#define M_OS_ALT_CTRL 3
+#define M_OS_WIN_CTRL 4
+#define M_OS_WIN_OPT 5
---
+++ src/behaviors/behavior_meteorite_custom_key.c
@@ -0,0 +1,139 @@
+#define DT_DRV_COMPAT zmk_behavior_meteorite_custom_key
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/meteorite_custom_keys.h>
+#include <drivers/behavior.h>
+#include <zmk/behavior.h>
+#include <zmk/custom_feature.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+struct behavior_meteorite_custom_key_data {
+    struct zmk_behavior_binding pressed_binding;
+    bool pressed;
+};
+
+static uint32_t keycode_for_param(uint32_t param, bool is_mac) {
+    switch (param) {
+    case M_OS_CTRL_CMD:
+        return is_mac ? LGUI : LCTRL;
+    case M_OS_ALT_OPT:
+        return LALT;
+    case M_OS_ALT_CTRL:
+        return is_mac ? LCTRL : LALT;
+    case M_OS_WIN_CTRL:
+        return is_mac ? LCTRL : LGUI;
+    case M_OS_WIN_OPT:
+        return is_mac ? LALT : LGUI;
+    default:
+        return 0;
+    }
+}
+
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata param1_values[] = {
+    {
+        .display_name = "OS Ctrl/Cmd",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = M_OS_CTRL_CMD,
+    },
+    {
+        .display_name = "OS Alt/Opt",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = M_OS_ALT_OPT,
+    },
+    {
+        .display_name = "OS Alt/Ctrl",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = M_OS_ALT_CTRL,
+    },
+    {
+        .display_name = "OS Win/Ctrl",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = M_OS_WIN_CTRL,
+    },
+    {
+        .display_name = "OS Win/Opt",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = M_OS_WIN_OPT,
+    },
+};
+
+static const struct behavior_parameter_metadata_set param_set = {
+    .param1_values = param1_values,
+    .param1_values_len = ARRAY_SIZE(param1_values),
+};
+
+static const struct behavior_parameter_metadata metadata = {
+    .sets_len = 1,
+    .sets = &param_set,
+};
+
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    struct behavior_meteorite_custom_key_data *data =
+        (struct behavior_meteorite_custom_key_data *)zmk_behavior_get_binding(binding->behavior_dev)
+            ->data;
+
+    if (data->pressed) {
+        LOG_ERR("Can't press the same meteorite custom key twice");
+        return -ENOTSUP;
+    }
+
+    bool is_mac = zmk_custom_config_os_is_mac();
+    uint32_t keycode = keycode_for_param(binding->param1, is_mac);
+    if (keycode == 0) {
+        LOG_ERR("Unknown meteorite custom key param: %u", binding->param1);
+        return -ENOTSUP;
+    }
+
+    data->pressed_binding.behavior_dev = DEVICE_DT_NAME(DT_NODELABEL(kp));
+    data->pressed_binding.param1 = keycode;
+    data->pressed_binding.param2 = 0;
+    data->pressed = true;
+
+    return zmk_behavior_invoke_binding(&data->pressed_binding, event, true);
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    struct behavior_meteorite_custom_key_data *data =
+        (struct behavior_meteorite_custom_key_data *)zmk_behavior_get_binding(binding->behavior_dev)
+            ->data;
+
+    if (!data->pressed) {
+        LOG_ERR("Meteorite custom key already released");
+        return -ENOTSUP;
+    }
+
+    data->pressed = false;
+    return zmk_behavior_invoke_binding(&data->pressed_binding, event, false);
+}
+
+static const struct behavior_driver_api behavior_meteorite_custom_key_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .parameter_metadata = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+};
+
+#define MCK_INST(n)                                                                               \
+    static struct behavior_meteorite_custom_key_data behavior_meteorite_custom_key_data_##n = {}; \
+    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, &behavior_meteorite_custom_key_data_##n, NULL,         \
+                            POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                     \
+                            &behavior_meteorite_custom_key_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(MCK_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
