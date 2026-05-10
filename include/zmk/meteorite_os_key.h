8aa15d845801f118a9dbe094550364959b4357a5
iwk7273 <iwk.nov16@gmail.com>
2026-05-10 14:30:35 +0900

    Refactor Meteorite OS key metadata macros

--- include/zmk/meteorite_os_key.h
+++ include/zmk/meteorite_os_key.h
@@ -13,93 +13,56 @@
 #include <dt-bindings/zmk/keys.h>
 #include <dt-bindings/zmk/meteorite_custom_keys.h>
 
+#define METEORITE_OS_KEY_SEP_NONE()
+#define METEORITE_OS_KEY_SEP_COMMA() ,
+
+#define METEORITE_OS_KEY_LIST_TAIL(X, ARG, SEP)                                                    \
+    X(ARG, M_OS_ALT_OPT, "Alt/Opt", LALT, LALT) SEP()                                             \
+    X(ARG, M_OS_ALT_CTRL, "Alt/Ctrl", LALT, LCTRL) SEP()                                          \
+    X(ARG, M_OS_WIN_CTRL, "Win/Ctrl", LGUI, LCTRL) SEP()                                          \
+    X(ARG, M_OS_WIN_OPT, "Win/Opt", LGUI, LALT) SEP()                                             \
+    X(ARG, M_OS_ALT_CMD, "Alt/Cmd", LALT, LGUI)
+
+#define METEORITE_OS_KEY_LIST(X, ARG, SEP)                                                        \
+    X(ARG, M_OS_CTRL_CMD, "Ctrl/Cmd", LCTRL, LGUI) SEP()                                          \
+    METEORITE_OS_KEY_LIST_TAIL(X, ARG, SEP)
+
+#define METEORITE_OS_KEY_LIST_EXCLUDING_CTRL_CMD(X, ARG, SEP)                                      \
+    METEORITE_OS_KEY_LIST_TAIL(X, ARG, SEP)
+
 static inline bool meteorite_os_keycode_for_param(uint32_t param, bool is_mac, uint32_t *keycode) {
     if (param == 0) {
         param = M_OS_CTRL_CMD;
     }
 
+#define METEORITE_OS_KEYCODE_CASE(_arg, id, display, win_key, mac_key)                            \
+    case id:                                                                                      \
+        *keycode = is_mac ? (mac_key) : (win_key);                                                 \
+        return true;
+
     switch (param) {
-    case M_OS_CTRL_CMD:
-        *keycode = is_mac ? LGUI : LCTRL;
-        return true;
-    case M_OS_ALT_OPT:
-        *keycode = LALT;
-        return true;
-    case M_OS_ALT_CTRL:
-        *keycode = is_mac ? LCTRL : LALT;
-        return true;
-    case M_OS_WIN_CTRL:
-        *keycode = is_mac ? LCTRL : LGUI;
-        return true;
-    case M_OS_WIN_OPT:
-        *keycode = is_mac ? LALT : LGUI;
-        return true;
-    case M_OS_ALT_CMD:
-        *keycode = is_mac ? LGUI : LALT;
-        return true;
+        METEORITE_OS_KEY_LIST(METEORITE_OS_KEYCODE_CASE, unused, METEORITE_OS_KEY_SEP_NONE)
     default:
         return false;
     }
+
+#undef METEORITE_OS_KEYCODE_CASE
 }
 
-#define METEORITE_OS_KEY_METADATA_VALUES(VAL_MACRO)                                               \
+#define METEORITE_OS_KEY_METADATA_VALUE(VAL_MACRO, id, display, win_key, mac_key)                 \
     {                                                                                             \
-        .display_name = "OS-Switch Mod Ctrl/Cmd",                                                 \
+        .display_name = "OS-Switch Mod " display,                                                 \
         .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                              \
-        .value = VAL_MACRO(M_OS_CTRL_CMD),                                                        \
-    },                                                                                            \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Alt/Opt",                                              \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_ALT_OPT),                                                     \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Alt/Ctrl",                                             \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_ALT_CTRL),                                                    \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Win/Ctrl",                                             \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_WIN_CTRL),                                                    \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Win/Opt",                                              \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_WIN_OPT),                                                     \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Alt/Cmd",                                              \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_ALT_CMD),                                                     \
-        }
+        .value = VAL_MACRO(id),                                                                   \
+    }
+
+#define METEORITE_OS_KEY_METADATA_VALUES(VAL_MACRO)                                               \
+    METEORITE_OS_KEY_LIST(METEORITE_OS_KEY_METADATA_VALUE, VAL_MACRO,                             \
+                          METEORITE_OS_KEY_SEP_COMMA)
 
 #define METEORITE_OS_KEY_METADATA_VALUES_EXCLUDING_CTRL_CMD(VAL_MACRO)                             \
-    {                                                                                             \
-        .display_name = "OS-Switch Mod Alt/Opt",                                                  \
-        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                              \
-        .value = VAL_MACRO(M_OS_ALT_OPT),                                                         \
-    },                                                                                            \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Alt/Ctrl",                                             \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_ALT_CTRL),                                                    \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Win/Ctrl",                                             \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_WIN_CTRL),                                                    \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Win/Opt",                                              \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_WIN_OPT),                                                     \
-        },                                                                                        \
-        {                                                                                         \
-            .display_name = "OS-Switch Mod Alt/Cmd",                                              \
-            .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,                                          \
-            .value = VAL_MACRO(M_OS_ALT_CMD),                                                     \
-        }
+    METEORITE_OS_KEY_LIST_EXCLUDING_CTRL_CMD(METEORITE_OS_KEY_METADATA_VALUE, VAL_MACRO,           \
+                                             METEORITE_OS_KEY_SEP_COMMA)
 
 #define METEORITE_OS_KEY_METADATA_VALUES_WITH_DEFAULT(DEFAULT_VAL, VAL_MACRO)                     \
     {                                                                                             \
